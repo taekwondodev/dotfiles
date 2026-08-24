df898f01ea61f57ea7561def5e879f25b01faa75
taekwondodev <taekwondodev@outlook.it>
2026-08-24 16:35:27 +0200

    chore: point default shell working dir at ~/Developer

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -15,8 +15,8 @@ fish_add_path ~/.cargo/bin
 fish_add_path ~/go/bin
 
 # Working Directory
-if status is-interactive && test "$TERM_PROGRAM" != zed && test -d ~/Desktop
-    cd ~/Desktop
+if status is-interactive && test -d ~/Developer
+    cd ~/Developer
 end
 
 # Python Virtual Environment Auto Activate
